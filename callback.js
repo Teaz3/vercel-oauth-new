448d243b68009fb4f9526f919d589c374f389974
Teaz3 <164876264+Teaz3@users.noreply.github.com>
2024-09-10 02:38:35 +0100

    Added home route and callback route

--- callback.js
+++ callback.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Home route for the root path "/"
+app.get('/', (req, res) => {
+  res.send('Welcome to the OAuth App!');
+});
+
+// Callback route for OAuth
 app.get('/callback', (req, res) => {
   res.send('OAuth callback received!');
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
